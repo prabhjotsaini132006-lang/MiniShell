a7c4ecadfa4beb929856a906cee9dc41be4a7bb9
Prabhjot Saini <prabhjotsaini132006@gmail.com>
2026-09-19 15:36:02 +0000

    Implement interactive shell loop

--- src/main.c
+++ src/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
-    printf("MiniShell started!\n");
+    char *input = NULL;
+    size_t size = 0;
+
+    while(1){
+        printf("minishell> ");
+        fflush(stdout);
+
+        if(getline(&input, &size, stdin) == -1)
+        {
+            printf("\n");
+            break;
+        }
+
+        printf("You entered: %s", input);
+    }
+  
+    free(input);
 
     return 0;
 }
